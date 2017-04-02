4af04963eb762ee169ec31d0ce2c426dece02131
Ram Goli <ramsgoli@gmail.com>
2017-04-02 05:24:55 -0700

    switched to dirname

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ bot.dialog('/', [
 
         var execFile = require('child_process').execFile;
 
-        execFile('./src/SilioNavTest',['./src/locationdata.txt', session.userData.lat, session.userData.lon, session.userData.cost, session.userData.distance], function(err, data) {
+        execFile(__dirname + '/src/SilioNavTest',[__dirname + '/src/locationdata.txt', session.userData.lat, session.userData.lon, session.userData.cost, session.userData.distance], function(err, data) {
             session.send("We in here!");
             if(err) {
                 session.send(err.toString());
