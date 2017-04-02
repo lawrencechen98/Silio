ae74972803097ee0463dd676e7e1287397629085
Ram Goli <ramsgoli@gmail.com>
2017-04-02 04:22:06 -0700

    renamed helloworld to server

---
+++ server.js
@@ -0,0 +1,57 @@
+var builder = require('botbuilder');
+var restify = require('restify');
+var locationDialog = require('botbuilder-location');
+var config = require('./config');
+
+ var connector = new builder.ChatConnector({
+     appId: config.BOT_ID,
+     appPassword: config.BOT_PASSWORD
+});
+
+var bot = new builder.UniversalBot(connector);
+
+bot.dialog('/', [
+   function (session) {
+        //builder.Prompts.text(session, "Hello... Where are you?");
+        var options = {
+            prompt: "Where do you want to leave from?",
+            useNativeControl: true
+        };
+        locationDialog.getLocation(session, options);
+        builder.Prompts.text(session, "Hello... What's your latitude?");
+    },
+    function (session, results) {
+        session.userData.lat = results.response;
+        builder.Prompts.text(session, "Okay, now to what's your longitude?"); 
+    },
+    function (session, results) {
+        session.userData.lon = results.response;
+        builder.Prompts.text(session, "How much do you want to spend?"); 
+    },
+    function (session, results) {
+        session.userData.cost = results.response;
+        builder.Prompts.text(session, "How far are you willing to drive in total?"); 
+    },
+    function (session, results) {
+        session.userData.distance = results.response;
+
+
+        var execFile = require('child_process').execFile;
+
+        execFile('./src/SilioNavTest',['./src/locationdata.txt', session.userData.lat, session.userData.lon, session.userData.cost, session.userData.distance], function(err, data) {
+            if(err) {
+                console.log(err)
+            }else
+                session.send(data);                       
+        }); 
+        session.send("Perfect. Let me plan your night:");
+    }/
+]);
+
+var server = restify.createServer();
+
+server.post('/api/messages', connector.listen());
+
+server.listen(process.env.port || 3978, function () {
+        console.log('%s listening to %s', server.name, server.url); 
+});
