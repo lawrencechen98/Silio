f8aed8fe383df45120388e26469ccfeae4d2b08c
Ram Goli <ramsgoli@gmail.com>
2017-04-02 05:01:56 -0700

    shit works

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    BING_MAPS_API_KEY: gSnAT_zqGCnjqw0-DDBjLqd-MWpxUj6dWCoLXMuaGyJXx7R5XHCyXAmDYe0v8zp,
-    BOT_ID: f9eef841-e41f-4022-ab22-2ad7ea840755,
-    BOT_PASSWORD: BXwLXXAoxYjniWVeHgL9LE0,
-}
