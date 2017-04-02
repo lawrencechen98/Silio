fe5eafe101e363d64e3f45e86c425fc07e6b09e0
Ram Goli <ramsgoli@gmail.com>
2017-04-02 05:50:25 -0700

    more ./

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ bot.dialog('/', [
 
         var execFile = require('child_process').execFile;
 
-        execFile('./SilioNavTest', ['locationdata.txt', session.userData.lat, session.userData.lon, session.userData.cost, session.userData.distance], function(err, data) {
+        execFile('./SilioNavTest', ['./locationdata.txt', session.userData.lat, session.userData.lon, session.userData.cost, session.userData.distance], function(err, data) {
             if(err) {
                 session.send(err.toString());
                 console.log(err)
